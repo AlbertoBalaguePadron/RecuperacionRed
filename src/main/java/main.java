c6b93bdf579ff0fbe6d7de621ed56efad4fcc817
Alberto <alberto01bp@gmail.com>
2022-03-08 10:26:08 +0000

    Feature: creo el archivo Dockerfile, en el maín inicializo la sesion, el canal y el sacanner

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,18 @@
-public class main º{
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.Session;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[]args) {
+
+        Session ss = null;
+        ChannelExec canal = null;
+        Scanner leer = new Scanner(System.in);
+
+
+
+
+    }
 }
