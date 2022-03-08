6db35417954783c28bbe4584601a809db86657b6
Alberto <alberto01bp@gmail.com>
2022-03-08 10:57:07 +0000

    Feature: pido nombre de ficheros y los creo con su contenido

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,6 +3,7 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class main {
@@ -15,16 +16,16 @@ public class main {
 
         try {
 
-            System.out.println("Introduce la ip");
+            System.out.println("Introduce la IP");
             String ip = leer.next();
 
-            System.out.println("Introduce el puerto");
+            System.out.println("Introduce el Puerto");
             int puerto = leer.nextInt();
 
-            System.out.println("Introduce el usuario");
+            System.out.println("Introduce el Usuario");
             String usuario = leer.next();
 
-            System.out.println("Introduce la contraseña");
+            System.out.println("Introduce la Contraseña");
             String contra = leer.next();
 
             sesion = new JSch().getSession(usuario, ip ,puerto);
@@ -33,12 +34,39 @@ public class main {
             sesion.connect();
             System.out.println("Conectado correctamente");
 
+            int bucle = 0;
+
+            while (bucle == 0) {
+
+                canal = (ChannelExec) sesion.openChannel("exec");
+
+                System.out.println("Introduce el nombre del archivo");
+                String nombreArchivo = leer.next();
+
+                System.out.println("Introduce la fecha y hora");
+                String fechaHora = leer.next();
+
+                canal.setCommand("touch /home/" + nombreArchivo);
+                canal.setCommand("echo " + fechaHora + " > " + nombreArchivo);
+
+                ByteArrayOutputStream respuesta = new ByteArrayOutputStream();
+                canal.setOutputStream(respuesta);
+
+                canal.connect();
+
+                while (canal.isConnected()) {
+                    Thread.sleep(100);
+                }
 
 
+                String respuestaString = new String(respuesta.toByteArray());
+                System.out.println(respuestaString);
 
+                System.out.println("Para introducir otro fichero pulse 0, para salir pulse 1 ");
+                bucle = leer.nextInt();
+            }
 
-
-        } catch (JSchException e) {
+        } catch (JSchException | InterruptedException e) {
             e.printStackTrace();
         } finally {
             if(sesion != null) sesion.disconnect();
